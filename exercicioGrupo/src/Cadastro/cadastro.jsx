243347f69951fa5f779c29d752da583b9c61e4b2
Kauanny19 <kauannycristina01n@gmail.com>
2025-02-05 13:39:58 -0300

    Exercício Grupo - Cadastro

---
+++ exercicioGrupo/src/Cadastro/cadastro.jsx
@@ -0,0 +1,102 @@
+import * as React from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import LockOutLinedIcon from "@mui/icons-material/LockOutLined";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
+
+function Cadastro() {
+  const [user, setUser] = useState({
+    nome: "",
+    age: "",
+    email: "",
+    password: "",
+  });
+
+  const onChange = (event) => {
+    const { name, value } = event.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(
+      `Nome:  ${user.name} Idade: ${user.age} Email: ${user.email} Senha: ${user.password}`
+    );
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ margin: 1, backgroundColor: "#ffce30" }}>
+          <LockOutLinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Vio
+        </Typography>
+        <Box component="form" sx={{ mt: 1 }} onSubmit={handleSubmit} noValidate>
+          <TextField
+            required
+            fullWidth
+            id="name"
+            label="Nome"
+            name="name"
+            margin="normal"
+            value={user.name}
+            onChange={onChange}
+          />
+          <TextField
+            required
+            fullWidth
+            id="age"
+            label="Idade"
+            name="age"
+            margin="normal"
+            value={user.age}
+            onChange={onChange}
+          />
+          <TextField
+            required
+            fullWidth
+            id="email"
+            label="Email"
+            name="email"
+            margin="normal"
+            value={user.email}
+            onChange={onChange}
+          />
+          <TextField
+            required
+            fullWidth
+            id="password"
+            label="Senha"
+            name="password"
+            margin="normal"
+            type="password"
+            value={user.password}
+            onChange={onChange}
+          />
+          <Button
+            sx={{ mt: 3, mb: 2, backgroundColor: "#ffce30" }}
+            fullWidth
+            type="submit"
+            variant="contained"
+          >
+            Entrar
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
+export default Cadastro;
